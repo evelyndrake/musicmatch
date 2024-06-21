08fae4965e8e4ce192100532999a75de3a7e397c
Evelyn Drake <128602777+evelyndrake@users.noreply.github.com>
2024-06-21 17:04:44 -0400

    Added fly-in for next card

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Player from './Player';
 import {useSwipeable} from 'react-swipeable';
 
@@ -6,6 +6,7 @@ const Card = ({title, videoId, swipeLeft, swipeRight}) => {
     const [swipeAmount, setSwipeAmount] = useState(0);
     const [swipeAmountY, setSwipeAmountY] = useState(0);
     const [isDragging, setIsDragging] = useState(false);
+    const [animateCard, setAnimateCard] = useState(false);
     const swipeThreshold = 200;
 
     const handlers = useSwipeable({
@@ -34,10 +35,18 @@ const Card = ({title, videoId, swipeLeft, swipeRight}) => {
             setSwipeAmount(0);
             setSwipeAmountY(0);
             setIsDragging(false);
+            
         },
         preventDefaultTouchmoveEvent: true,
         trackMouse: true
     });
+
+    useEffect(() => {
+        setAnimateCard(true);
+        console.log('Animating card')
+        setTimeout(() => setAnimateCard(false), 500);
+    }, [videoId]);
+
 
     return (
         <div
@@ -51,7 +60,7 @@ const Card = ({title, videoId, swipeLeft, swipeRight}) => {
                 borderStyle: (swipeAmount != 0 && Math.abs(swipeAmount) > swipeThreshold) ? 'solid' : 'none',
                 transition: isDragging ? 'none' : 'transform 0.2s ease-out' // Smooth transition only when not dragging
             }}
-            {...handlers} className={'card'}>
+            {...handlers} className={`card ${animateCard ? 'flyIn' : ''}`}>
                 <h2 className="title">{title}</h2>
                 <Player videoId={videoId} />
                 {/* <button onClick={swipeLeft}>Left Swipe</button>
